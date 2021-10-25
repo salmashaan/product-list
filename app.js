ba513a1b7a2e6a9d5831a85c99940fba76bd7760
salmashaan <smalmashaan@gmail.com>
2021-10-25 16:15:57 +0300

    create done delete not working

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ const products = require("./data");
 
 const app = express();
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+app.use(express.json());
 
 app.get("/api/products", (req, res) => {
   res.json(products);
+});
+
+app.post("/api/products", (req, res) => {
+  const newProduct = products.push(req.body);
+  res.status(201).json(newProduct);
+  console.log("POSTing", req.body);
+});
+
+app.delete("/api/products/:productId", (req, res) => {
+  const { productId } = req.params;
+  const foundProduct = products.find(productId);
+  if (foundProduct) {
+    products.filter(foundCake);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "Cake not found" });
+  }
+});
+
+app.listen(8000, () => {
+  console.log("The application is running on localhost:8000");
 });
