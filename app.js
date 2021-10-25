dd7cb186246d4ac5a08c66985a520480a171728d
salmashaan <smalmashaan@gmail.com>
2021-10-25 19:56:02 +0300

    mongoose set up done

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 let products = require("./data");
 const productRoutes = require("./apis/products/routes");
+const connectDB = require("./database");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
+
+connectDB();
 
 app.listen(8000, () => {
   console.log("The application is running on localhost:8000");
