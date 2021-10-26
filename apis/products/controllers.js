2492e0eaa3ccb10daf00d24119d18d023c8532e8
salmashaan <smalmashaan@gmail.com>
2021-10-26 18:57:34 +0300

    done task 2 of the day

--- apis/products/controllers.js
+++ apis/products/controllers.js
@@ -1,24 +1,38 @@
 let products = require("../../data");
+const Product = require("../../models/Product");
+const mongoose = require("mongoose");
 
-exports.productListFetch = (req, res) => {
-  return res.json(products);
+exports.productListFetch = async (req, res) => {
+  try {
+    const products = await Product.find();
+    return res.json(products);
+  } catch (error) {
+    return res.status(500).json({ message: "error" });
+  }
 };
 
-exports.productListCreate = (req, res) => {
-  products.push(req.body);
-  res.status(201);
-  return res.json(req.body);
+exports.productListCreate = async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.status(201);
+    return res.json(newProduct);
+  } catch (error) {
+    return res.status(500).json({ message: "error" });
+  }
 };
 
-exports.productListDelete = (req, res) => {
-  const { productId } = req.params;
-  const foundProduct = products.find((product) => product.id === +productId);
+exports.productListDelete = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    foundProduct = await Product.findById(productId);
 
-  if (foundProduct) {
-    products = products.filter((product) => product.id !== +foundProduct.id);
-
-    return res.status(204).end();
-  } else {
-    res.status(404).json({ message: "Product not found" });
+    if (foundProduct) {
+      const removeProduct = await Product.remove(foundProduct);
+      return res.status(204).json(removeProduct);
+    } else {
+      return res.status(404).json({ message: "This product doesn't exist" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "error" });
   }
 };
---
+++ models/Product.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  id: Number,
+  name: { type: String, required: true },
+  slug: String,
+  image: String,
+  description: String,
+  color: String,
+  quantity: { type: Number, min: 0 },
+  price: { type: Number, default: 10 },
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
